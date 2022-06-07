338f3b3335fbd57b053b16b8519d669361aead0a
Aowar <alex.zwer@mail.ru>
2022-06-07 21:10:14 +0300

    init commit

--- src/org/learn/app/App.java
+++ src/org/learn/app/App.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class App {
     public static void main(String[] args) {
         try {
